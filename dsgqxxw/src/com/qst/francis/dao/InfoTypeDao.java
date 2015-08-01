e0e2fe94085506ccfe8901c098556dc7b2cac3ad
francisXKF <xkf1273@gmail.com>
2015-08-01 17:47:35 +0800

    Revert "Revert "final work""
    This reverts commit 5450aa4bbda7cdcd7989912bf7a62010f4a55477.

---
+++ dsgqxxw/src/com/qst/francis/dao/InfoTypeDao.java
@@ -0,0 +1,5 @@
+package com.qst.francis.dao;
+
+public interface InfoTypeDao {
+	public String queryById(int id);
+}
---
+++ dsgqxxw/src/com/qst/francis/pojo/User.java
@@ -0,0 +1,41 @@
+package com.qst.francis.pojo;
+
+public class User {
+	private int user_id;
+	private String user_name;
+	private String user_password;
+	private int user_type_id;
+	private int user_state;
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_password() {
+		return user_password;
+	}
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+	public int getUser_type_id() {
+		return user_type_id;
+	}
+	public void setUser_type_id(int user_type_id) {
+		this.user_type_id = user_type_id;
+	}
+	public int getUser_state() {
+		return user_state;
+	}
+	public void setUser_state(int user_state) {
+		this.user_state = user_state;
+	}
+	
+}
